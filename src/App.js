6c3f0c8a9b13d21b634b8b7538b9e01a28c2b0bd
kauandemello97@gmail.com <kauandemello97@gmail.com>
2023-03-26 21:56:23 -0300

    UserContext imported and working

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import SubScriptions from "./pages/SubScriptions"
 import SubScriptionsID from "./pages/SubScriptionsID"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePlan from "./pages/HomePlan"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { useState, useEffect } from "react"
+import {UserContext} from "./contexts/UserContext"
 
-export default function App(){
-    
-
-    return(
-        <BrowserRouter>
-        <Routes>
-                <Route path= "/" element={<Login />}/>
-                <Route path= "/sign-up" element={<Signup />}/>
-                <Route path="/subscriptions" element={<SubScriptions />}/>
-                <Route path="/subscriptions/ID_DO_PLANO" element={<SubScriptionsID />}/>
-                <Route path="/home" element={<HomePlan />}/>
-            </Routes>
-        </BrowserRouter>
-    )
-
-}
+export default function App() {
+    const [user, setUser] = useState(null);
+  
+    useEffect(() => {
+      const credentials = JSON.parse(localStorage.getItem("credentials"));
+      if (credentials) {
+        setUser(credentials);
+      }
+    }, []);
+  
+    return (
+      <BrowserRouter>
+        <UserContext.Provider value={{ user, setUser }}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/sign-up" element={<Signup />} />
+            <Route path="/subscriptions" element={<SubScriptions />} />
+            <Route
+              path="/subscriptions/ID_DO_PLANO"
+              element={<SubScriptionsID />}
+            />
+            <Route path="/home" element={<HomePlan />} />
+          </Routes>
+        </UserContext.Provider>
+      </BrowserRouter>
+    );
+  }
